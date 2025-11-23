5ec5e92e7c69779c077895e352cd8c7c1cfc8e80
pulakhadamumamaitreyi <160453397+pulakhadamumamaitreyi@users.noreply.github.com>
2025-11-23 19:53:41 +0530

    Create User model with email and password fields

---
+++ User.js
@@ -0,0 +1,9 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/db");
+
+const User = sequelize.define("User", {
+  email: { type: DataTypes.STRING, unique: true },
+  password: DataTypes.STRING,
+});
+
+module.exports = User;
